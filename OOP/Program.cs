ec336ad5cdda5fd4783ba7601954243e82d5db96
Shohjahon727 <135617343+Shohjahon727@users.noreply.github.com>
2023-07-03 14:28:07 +0500

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using OOP.Abstraction.Abstract_Key_Word;
+
+var cat = new Cat();
+cat.MakeSound();
+cat.Sleep();
+
+var dog = new Dog();
+dog.MakeSound();
+dog.Sleep();
+
+var ant = new Ant();
+ant.MakeSound();
+ant.Sleep();
+Console.WriteLine();
